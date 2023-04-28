91124c2bb016756f35fb77892a238f811dbd894e
vincentpham123 <vincentpham32@gmail.com>
2023-04-27 17:14:40 -0700

    prereq

--- sql-associations/app/models/course.rb
+++ sql-associations/app/models/course.rb
@@ -14,12 +14,20 @@ class Course < ApplicationRecord
     # a Course will have many users/user
     #  Has many Users thru Enrollment table
     # via course_id
-    has_many :sign_ups,
+    has_many :sign_ups, #this builds an association to enrollment
         primary_key: :id,
         foreign_key: :course_id,
         class_name: :Enrollment
 
     has_many :students,
-        through: :sign_ups,
-        source: :student
+        through: :sign_ups, #this sepcifies that we want to fethc the associated students through the :sign_up table
+        source: :student # 
+    
+    # each course will have only one prerequisite 
+    has_one :prerequisite,
+        primary_key: :prereq_id,
+        foreign_key: :id,
+        class_name: :Course 
+    
+    
 end
